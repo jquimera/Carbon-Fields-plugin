c084c5bf37eccbb819827bc84857788db98143f4
Kaloyan Yosifov <kaloqn665@gmail.com>
2017-11-26 15:06:00 +0200

    The End

--- functions.php
+++ functions.php
@@ -1,7 +1,40 @@
 <?php
 
-
 if ( ! function_exists( 'crb_initilize_carbon_fields' ) ) {
+
+	$template_dir = get_template_directory_uri();
+
+	function crb_get_custom_post_name_array( $post_name ) {
+		$plural_name = $post_name . 's';
+
+		$labels = array( 
+	        'singular_name' => __( $post_name, 'crb' ),
+	        'add_new' => __( 'Add New ', 'crb' ),
+	        'add_new_item' => __( 'Add New ' . $post_name, 'crb' ),
+	        'edit_item' => __( 'Edit ' . $post_name, 'crb' ),
+	        'new_item' => __( 'New ' . $post_name, 'crb' ),
+	        'view_item' => __( 'View ' . $post_name, 'crb' ),
+	        'view_items' => __( 'View ' . $plural_name, 'crb' ),
+	        'search_items' => __( 'Search ' . $plural_name, 'crb' ),
+	        'not_found' => __( 'No ' . $plural_name . 'found', 'crb' ),
+	        'not_found_in_trash' => __( 'No ' . $plural_name . ' found in trash.' ),
+	        'parent_item_colon' => array( null, __('Parent Page:') ),
+	        'all_items' => __( 'All ' . $plural_name, 'crb' ),
+	        'archives' => __( $plural_name . ' Archives', 'crb' ),
+	        'attributes' => __( $post_name . ' Attributes', 'crb' ),
+	        'insert_into_item' => __( 'Insert into ' . $post_name, 'crb' ),
+	        'uploaded_to_this_item' => __( 'Uploaded to this ' . $post_name, 'crb' ),
+	        'featured_image' => array( _x( 'Featured Image', 'post' ), _x( 'Featured Image', 'page' ) ),
+	        'set_featured_image' => array( _x( 'Set featured image', 'post' ), _x( 'Set featured image', 'page' ) ),
+	        'remove_featured_image' => array( _x( 'Remove featured image', 'post' ), _x( 'Remove featured image', 'page' ) ),
+	        'use_featured_image' => array( _x( 'Use as featured image', 'post' ), _x( 'Use as featured image', 'page' ) ),
+	        'filter_items_list' => __( 'Filter ' . $plural_name . 'list', 'crb' ),
+	        'items_list_navigation' => __( $plural_name . ' navigation list', 'crb' ),
+	        'items_list' => __( $plural_name . ' list', 'crb' ) );
+
+		return $labels;
+	}
+
 	function crb_initilize_carbon_fields() {
 		include_once( 'vendor/autoload.php' );
 
@@ -16,4 +49,41 @@ if ( ! function_exists( 'crb_initilize_carbon_fields' ) ) {
 	}
 
 	add_action( 'carbon_fields_register_fields', 'crb_initialize_options' );
+
+	wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'theme-custom-script', get_template_directory_uri() . '/functions.js', array( 'jquery' ) );
+
+	function crb_init_scripts_and_styles() {
+		wp_deregister_script( 'theme-custom-script' );
+	}
+
+	add_action( 'wp_enqueue_scripts', 'crb_init_scripts_and_styles' );
+
+	function crb_init_post_types() {
+		$posts = carbon_get_theme_option( 'crb_posts' );
+
+		if ( empty( $posts ) ) {
+			return;
+		}
+
+
+		foreach ( $posts as $post ) {
+			foreach ( $post['options'] as $p ) {
+				var_dump( $p );
+			}
+			$custom_post = $post[ 'options' ];
+			$labels = crb_get_custom_post_name_array( esc_attr( $post['name'] ) );
+
+			$custom_post[] = $labels;
+			$custom_post['rewrite'] = array(
+				'slug' => $post['slug']
+			);
+
+			register_post_type( 'crb_' . strtolower( $post['name'] ), $custom_post );
+		}
+
+		flush_rewrite_rules();
+	}
+
+	add_action( 'carbon_fields_register_fields', 'crb_init_post_types' );
 }
--- options/theme-options.php
+++ options/theme-options.php
@@ -18,5 +18,45 @@ Container::make( 'theme_options', __( 'Custom Creator', 'crb' ) )
 			) )
 			->add_fields( array(
 				Field::make( 'text', 'name', __( 'Name', 'crb' ) ),
+				Field::make( 'text', 'slug', __( 'Slug', 'crb' ) ),
+				Field::make( 'set', 'options', __( 'Options', 'crb' ) )
+					->add_options( array(
+						'public' => __( 'Public', 'crb' ),
+						'show_ui' => __( 'Show in UI', 'crb' ),
+						'exclude_form_search' => __( 'Exclude From Search', 'crb' ),
+						'show_in_menu' => __( 'Show in Admin Menu', 'crb' ),
+						'show_in_admin_bar' => __( 'Show in Admin Bar', 'crb' ),
+						'show_in_nav_menus' => __( 'Show in Nav Menus', 'crb' ),
+						'has_archive' => __( 'Create an Archive', 'crb' ),
+						'with_front' => __( 'With Front of Page', 'crb' ),
+					) ),
 			) ),
 	) );
+
+
+/*
+'labels' => array( _x('Posts', 'post type general name'), _x('Pages', 'post type general name') ),
+        'singular_name' => array( _x('Post', 'post type singular name'), _x('Page', 'post type singular name') ),
+        'add_new' => array( _x('Add New', 'post'), _x('Add New', 'page') ),
+        'add_new_item' => array( __('Add New Post'), __('Add New Page') ),
+        'edit_item' => array( __('Edit Post'), __('Edit Page') ),
+        'new_item' => array( __('New Post'), __('New Page') ),
+        'view_item' => array( __('View Post'), __('View Page') ),
+        'view_items' => array( __('View Posts'), __('View Pages') ),
+        'search_items' => array( __('Search Posts'), __('Search Pages') ),
+        'not_found' => array( __('No posts found.'), __('No pages found.') ),
+        'not_found_in_trash' => array( __('No posts found in Trash.'), __('No pages found in Trash.') ),
+        'parent_item_colon' => array( null, __('Parent Page:') ),
+        'all_items' => array( __( 'All Posts' ), __( 'All Pages' ) ),
+        'archives' => array( __( 'Post Archives' ), __( 'Page Archives' ) ),
+        'attributes' => array( __( 'Post Attributes' ), __( 'Page Attributes' ) ),
+        'insert_into_item' => array( __( 'Insert into post' ), __( 'Insert into page' ) ),
+        'uploaded_to_this_item' => array( __( 'Uploaded to this post' ), __( 'Uploaded to this page' ) ),
+        'featured_image' => array( _x( 'Featured Image', 'post' ), _x( 'Featured Image', 'page' ) ),
+        'set_featured_image' => array( _x( 'Set featured image', 'post' ), _x( 'Set featured image', 'page' ) ),
+        'remove_featured_image' => array( _x( 'Remove featured image', 'post' ), _x( 'Remove featured image', 'page' ) ),
+        'use_featured_image' => array( _x( 'Use as featured image', 'post' ), _x( 'Use as featured image', 'page' ) ),
+        'filter_items_list' => array( __( 'Filter posts list' ), __( 'Filter pages list' ) ),
+        'items_list_navigation' => array( __( 'Posts list navigation' ), __( 'Pages list navigation' ) ),
+        'items_list' => array( __( 'Posts list' ), __( 'Pages list' ) ),
+*/
